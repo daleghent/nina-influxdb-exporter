04a251979ddb19a1a1686b2ec7aee70fef372f86
Dale Ghent <daleg@elemental.org>
2023-08-12 01:10:45 -0400

    Add link for plugin icon

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -44,7 +44,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ChangelogURL", "https://github.com/daleghent/nina-influxdb-exporter/blob/main/CHANGELOG.md")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
-//[assembly: AssemblyMetadata("FeaturedImageURL", "https://daleghent.github.io/nina-plugins/assets/images/influxdb-exporter.png")]
+[assembly: AssemblyMetadata("FeaturedImageURL", "https://daleghent.github.io/nina-plugins/assets/images/influxdb-logo.png")]
 //[Optional] A url to an example screenshot of your plugin in action
 [assembly: AssemblyMetadata("ScreenshotURL", "")]
 //[Optional] An additional url to an example example screenshot of your plugin in action

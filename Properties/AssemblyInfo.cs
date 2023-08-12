522276c7f3b489ebdb2be3ba3a9d81799f86b581
Dale Ghent <daleg@elemental.org>
2023-08-12 00:05:01 -0400

    Set minimum version to 3.0.0.1032

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2023 Dale Ghent")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.1031")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.1032")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
